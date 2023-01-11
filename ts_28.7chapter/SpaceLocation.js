6fbd5d98332b87d45ba15b38b0a514c74f654952
xSorArc <87716789+xSorArc@users.noreply.github.com>
2023-01-10 21:52:09 -0600

    Completed chapter 28 exercises

---
+++ ts_28.7chapter/SpaceLocation.js
@@ -0,0 +1,11 @@
+"use strict";
+exports.__esModule = true;
+exports.SpaceLocation = void 0;
+var SpaceLocation = /** @class */ (function () {
+    function SpaceLocation(name, kilometersAway) {
+        this.name = name;
+        this.kilometersAway = kilometersAway;
+    }
+    return SpaceLocation;
+}());
+exports.SpaceLocation = SpaceLocation;
